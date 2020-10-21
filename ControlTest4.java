eed2abe8fc0fec601b08555ea6d32587d3b6696f
jooly14 <69379312+jooly14@users.noreply.github.com>
2020-10-21 16:47:31 +0900

    switch문에서 default 사용!! 2010.10.21

--- ControlTest4.java
+++ ControlTest4.java
@@ -13,16 +13,17 @@ public class ControlTest4{
 			case 'u':
 			case 'y':	System.out.println("모음");
 			break;
-			case 'b': case 'c': case 'd': case 'f': case 'g':
-			case 'h': case 'j': case 'k': case 'l': case 'm':
-			case 'n': case 'p': case 'q': case 'r': case 's':
-			case 't': case 'v': case 'w': case 'x': case 'z': System.out.println("자음");
+			default: System.out.println("자음");
 		
 		}
 		
 	}
 }
 /*
+case 'b': case 'c': case 'd': case 'f': case 'g':
+			case 'h': case 'j': case 'k': case 'l': case 'm':
+			case 'n': case 'p': case 'q': case 'r': case 's':
+			case 't': case 'v': case 'w': case 'x': case 'z':
 영문 알파벳 1개를 입력 받아 모음인지 자음인지 출력하는 프로그램
 입력 : a
 모음 <=  출력
