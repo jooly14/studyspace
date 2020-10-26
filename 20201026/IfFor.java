9811539cc3cb9b0c3a371ea0c852514247fe0700
jooly14 <69379312+jooly14@users.noreply.github.com>
2020-10-26 17:39:31 +0900

    20201026_오후_공백 수정

--- 20201026/IfFor.java
+++ 20201026/IfFor.java
@@ -52,7 +52,7 @@ public class IfFor{
 		System.out.println("=======================");
 		for(int i=0;i<5;i++){
 			for(int j=0;j<4-i;j++){
-				System.out.print("  ");
+				System.out.print(" ");
 			}
 			for(int k=0;k<i+1;k++){
 				System.out.print("*");
@@ -63,7 +63,7 @@ public class IfFor{
 		System.out.println("=======================");
 		for(int i =0;i<5;i++){
 			for(int j=0;j<i;j++){
-				System.out.print("  ");
+				System.out.print(" ");
 			}
 			for(int k=0;k<5-i;k++){
 				System.out.print("*");
@@ -73,7 +73,7 @@ public class IfFor{
 		System.out.println("=======================");
 		for(int i=0;i<5;i++){
 			for(int j=0;j<4-i;j++){
-				System.out.print("  ");
+				System.out.print(" ");
 			}
 			for(int k=0;k<i*2+1;k++){
 				System.out.print("*");
@@ -86,7 +86,7 @@ public class IfFor{
 		int u = sc.nextInt();
 		for(int i=0;i<u;i++){
 			for(int j=0;j<Math.abs(u/2-i);j++){
-				System.out.print("  ");
+				System.out.print(" ");
 			}
 			for(int k=u;k>(Math.abs(u/2-i))*2;k--){
 				System.out.print("*");
