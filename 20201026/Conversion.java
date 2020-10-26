6de4ca04e5d412832dad4865c08ab636f3e6d057
jooly14 <69379312+jooly14@users.noreply.github.com>
2020-10-26 12:55:45 +0900

    2020.10.26_지난주 복습_오전

---
+++ 20201026/Conversion.java
@@ -0,0 +1,13 @@
+public class Conversion{
+	public static void main(String args[]){
+		for(int i = 65;i<=90;i++){
+			System.out.println((char)i);
+			
+		}
+		
+		int x,y =0;
+		System.out.println(x);
+	
+		
+	}
+}
