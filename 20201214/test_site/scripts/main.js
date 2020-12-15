d5dd070bd27cb472e765d21d22b5ddb505ef712d
jooly14 <69379312+jooly14@users.noreply.github.com>
2020-12-15 17:56:39 +0900

    Add files via upload

--- 20201214/test_site/scripts/main.js
+++ 20201214/test_site/scripts/main.js
@@ -1,2 +1,11 @@
 let myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello World';
+
+let myImage = document.querySelector('img');
+myImage.onclick = function(){
+    if(myImage.getAttribute('src')==="images/smile.png"){
+        myImage.setAttribute('src',"images/soso.png");
+    }else{
+        myImage.setAttribute('src',"images/smile.png")
+    }
+}
