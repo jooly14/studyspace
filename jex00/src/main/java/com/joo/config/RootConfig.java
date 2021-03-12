d2ce0a6f0549cbf0564502557bd00fdd51a9f405
jooly14 <69379312+jooly14@users.noreply.github.com>
2021-03-12 17:42:33 +0900

    xml, 자바 설정 이용한 DI테스트 및 MariaDB연결

---
+++ jex00/src/main/java/com/joo/config/RootConfig.java
@@ -0,0 +1,10 @@
+package com.joo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages= {"com.joo.sample"})
+public class RootConfig {
+
+}
